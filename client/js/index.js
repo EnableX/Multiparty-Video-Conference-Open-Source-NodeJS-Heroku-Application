5f53ba117b2c11f2f4f6e493800cba331b3be56f
Rajesh Goyal <rajesh.g@vcloudx.com>
2020-11-18 10:17:50 +0530

    standardize the code between multiple nodejs repos

--- client/js/index.js
+++ client/js/index.js
@@ -94,7 +94,7 @@ var createRoom = function (callback) {
       }
     }
   };
-  xhttp.open('POST', '/createRoom/', true);
+  xhttp.open('POST', '/api/room/multi/', true);
   xhttp.setRequestHeader('Content-Type', 'application/json');
   xhttp.setRequestHeader('Authorization', `Basic ${btoa(`${username}:${password}`)}`);
   xhttp.send();
--- util/logger/logger.js
+++ util/logger/logger.js
@@ -1,40 +1,6 @@
 const log4js = require('log4js');
+const logFile = require('./log4js_configuration.json');
 
-global.config = global.config || {};
-
-global.config.logger = global.config.logger || {};
-
-log4js.configure({
-  appenders: {
-    appApi: {
-      type: 'file',
-      filename: 'logs/app.log',
-      maxLogSize: 1048576,
-      backups: 5,
-      layout: {
-        type: 'pattern',
-        pattern: '%d{ddMMyyhhmmss}::%p::%c::%m',
-        replaceConsole: true,
-      },
-    },
-  },
-  categories: { default: { appenders: ['appApi'], level: 'info' } },
-});
+log4js.configure(logFile);
 
 exports.logger = log4js;
-
-exports.logger.objectToLog = (jsonInput) => {
-  if (jsonInput === undefined) {
-    return '';
-  }
-  if (typeof (jsonInput) !== 'object') {
-    return jsonInput;
-  } if (jsonInput.constructor === Array) {
-    return '[Object]';
-  }
-  const jsonString = JSON.stringify(jsonInput, logJsonReplacer);
-  return jsonString.replace(/['"]+/g, '')
-    .replace(/[:]+/g, ': ')
-    .replace(/[,]+/g, ', ')
-    .slice(1, -1);
-};
